35564661eef2f85b853007148a5f5284cd5dde77
BryanPolio <hp240512@alumno.udb.edu.sv>
2025-08-26 19:50:10 -0600

    Add files via upload

---
+++ spring-api-rest/src/main/java/sv/edu/udb/SpringApiRestApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb; //BRYAN STEVEN HERNANDEZ POLIO HP240512
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+
+}
