29ea27792f910ef951f89da399dd55d4d4b20c53
BryanPolio <hp240512@alumno.udb.edu.sv>
2025-09-23 19:55:25 -0600

    Add files via upload

---
+++ spring-jwt-basic/src/main/java/sv/edu/udb/SpringJwtBasicApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb; //BRYAN STEVEN HERNANDEZ POLIO HP240512
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtBasicApplication.class, args);
+	}
+
+}
